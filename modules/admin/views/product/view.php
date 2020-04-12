ad87b7b9306ff133862902dbed169fe0077e7d06
Konstantin <woodprop@yandex.ru>
2020-04-13 00:14:31 +0300

    Admin: product detail view

--- modules/admin/views/product/view.php
+++ modules/admin/views/product/view.php
@@ -7,39 +7,56 @@ use yii\widgets\DetailView;
 /* @var $model app\modules\admin\models\Product */
 
 $this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Товары', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="product-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'category_id',
-            'title',
-            'content:ntext',
-            'price',
-            'old_price',
-            'description',
-            'keywords',
-            'img',
-            'is_offer',
-        ],
-    ]) ?>
-
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+            <?= Html::a('Изменить', ['update', 'id' => $model->id], ['class' => 'btn btn-warning btn-lg']) ?>
+            <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger btn-lg',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+            </div>
+            <div class="card-body">
+            <?= DetailView::widget([
+                'model' => $model,
+                'options' => [
+                    'class' => 'table table-bordered table-hover'
+                ],
+                'attributes' => [
+                    'id',
+                    'category_id',
+                    'title',
+                    'content:ntext',
+                    'price',
+                    'old_price',
+                    'description',
+                    'keywords',
+                    //'img',
+                    [
+                        'attribute' => 'img',
+                        'value' => function($data){
+                            return "@web/images/products/{$data->img}";
+                            },
+                        'format' => 'image',
+                    ],
+                    [
+                        'attribute' => 'is_offer',
+                        'value' => function($data){
+                            return $data->is_offer ? '<span class="text-success">ДА</span>' : '<span class="text-danger">НЕТ</span>';
+                        },
+                        'format' => 'html',
+                    ],
+                ],
+            ]) ?>
+            </div>
+        </div>
+    </div>
 </div>
